8bda6bb769d6a02644fa34b0f293a3f4d7cfb0eb
sanagbah-007 <s.anagbah@alustudent.com>
2026-07-21 16:02:31 +0000

    1st commit

---
+++ Cursed_Castel_Week3.py
@@ -0,0 +1,82 @@
+# The Cursed Castle of Pytheria is an adventure game where the player navigates through a castle, 
+# facing challenges and making choices that affect the outcome of the game. 
+
+# This is a introduction function that welcomes the player to the game
+# Asks for the player's name.                                
+def introduction():
+    print("\nYou were transported against your will, but you must survive!")
+    print("Welcome to the Cursed Castle of Pytheria!")
+    print ("You are on your own")
+
+
+
+# Challenge 1: The player must choose a path in the castle.
+# Create check_level(player_name, level). Use an if statement. If level >= 5, 
+# print 'The gateopens!'; otherwise print 'You are not strong enough!'
+def chose_level():
+    try:
+        # get player name and level
+        player_name = input("\nEnter your name: ")
+        level_str = input("Choose a level (number): ")
+        level = int(level_str)
+    except ValueError:
+        print("Invalid level. Please enter a number.")
+        return
+    except Exception:
+        print("An error occurred.")
+        return
+
+    # the nested function is here to check level as requested
+    def check_level(player_name, level):
+        if level >= 5:
+            print('The gateopens!')
+        else:
+            print('You are not strong enough!')
+
+    check_level(player_name, level)
+
+
+# Challenge 2 is meant to provide a menu for the user to chose a weapon.
+# choose_weapon(weapon). Use if/elif/else for sword, bow, staff and invalid weapon.
+def chose_weapon():
+    try:
+        weapon = input("\nChoose a weapon (sword/bow/staff): ")
+    except Exception:
+        print("An error occurred reading the weapon.")
+        return
+
+    if weapon == 'sword':
+        print('You swing the sword like a knight.\n')
+    elif weapon == 'bow':
+        print('You hold an arrow and take aim.\n')
+    elif weapon == 'staff':
+        print('You can open portals through your staff.\n')
+    else:
+        print('Invalid weapon.')
+        return chose_weapon()
+    return weapon
+
+
+# Challenge 3 is meant to reward a user after they open 10 chests
+# Use a for loop to open five treasure chests. Then create collect_gold(number_of_chests)
+# where each chest gives 10 gold coins.
+def treasure_chest():
+    for i in range(5):
+        print(f"\nOpening chest {i + 1}...")
+    
+    def collect_gold(number_of_chests):
+        gold = number_of_chests * 10
+        print(f"\nYou collected {gold} gold coins!\n")
+    
+    collect_gold(5)
+
+
+# Challenge 4
+
+introduction()
+
+chose_level()
+
+chose_weapon()
+
+treasure_chest()
